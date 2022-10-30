234aaf6df25b30f4109bfa3b6ebb0c90b12c13d0
Jason Toh <jasontoh.21@ichat.sp.edu.sg>
2022-10-30 16:10:48 +0800

    Created a list of temperature readings and calculated its average and maximum and minimum

--- Lab2.py
+++ Lab2.py
@@ -1,11 +1,36 @@
-def calculate_bmi(weight,height):
-    print("Height = " + str(height))
-    print("Weight = " + str(weight))
-    bmi = weight/(height*height)
-    if(bmi < 18.5):
-        return f'You have a BMI rating of {bmi}.You are underweight'
-    elif(bmi <=18.5 and bmi <=25):
-        return f'You have a BMI rating of{bmi}.You have a normal weight'
-    else:
-        return f'You have a BMI rating of {bmi}.You are overweight'
-print(calculate_bmi(57,1.73))
+def main():
+    print("ET0735 (DevOps for AIoT - Lab 2 - Introduction to Python")
+    display_main_menu()
+    num_list = get_user_input()
+    print(num_list)
+    print(calc_average_temperature(num_list))
+    print(calc_min_max_temperature(num_list))
+
+
+def display_main_menu():
+    print("Enter some numbers seperated by commas (e.g. 5,67,32)")
+
+
+def get_user_input():
+    user_input = input()  # Strings ""
+    user_input_split = user_input.split(",")
+    return [float(x) for x in user_input_split]
+
+
+def calc_average_temperature(num_list):
+    total = 0
+    for numbers in num_list:
+        total += numbers
+    average = total / len(num_list)
+    return f'The calculated average value of all temperature readings is {average}.'
+
+
+def calc_min_max_temperature(num_list):
+    maximum = max(num_list)
+    minimum = min(num_list)
+    print("The minimum and maximum temperature in the list is ");
+    return [int(minimum), int(maximum)]
+
+
+if __name__ == "__main__":
+    main()
