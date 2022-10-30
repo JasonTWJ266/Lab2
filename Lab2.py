42981612fece083321b5aa7b491736ede19b4fda
Jason Toh <jasontoh.21@ichat.sp.edu.sg>
2022-10-30 14:41:26 +0800

    Changes to Lab2

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,11 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def calculate_bmi(weight,height):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight/(height*height)
+    if(bmi < 18.5):
+        return f'You have a BMI rating of {bmi}.You are underweight'
+    elif(bmi <=18.5 and bmi <=25):
+        return f'You have a BMI rating of{bmi}.You have a normal weight'
+    else:
+        return f'You have a BMI rating of {bmi}.You are overweight'
+print(calculate_bmi(57,1.73))
